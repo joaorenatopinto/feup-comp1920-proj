edcec0e0a6ea6b4f5d615828fe828bb003eb6381
afonsoafonsoafonso <afonsomendonca91@gmail.com>
2020-04-16 16:58:44 +0100

    contas e o crlh

--- javacc/ASTAdd.java
+++ javacc/ASTAdd.java
@@ -12,19 +12,51 @@ class ASTAdd extends SimpleNode {
 
   @Override
   public int process() {
-    SimpleNode left = (SimpleNode)this.children[0];
-    SimpleNode right = (SimpleNode)this.children[1];
-    System.out.println(left.toString());
-    System.out.println(right.toString());
-    return left.process() + right.process();
-    /*
-    System.out.println(this.getClass());
-    if (this.children == null) return 1;
-    for(int i = 0; i < this.children.length; i++) {
-      ((SimpleNode)this.children[i]).process();
+    //SimpleNode left = (SimpleNode)this.children[0];
+    //SimpleNode right = (SimpleNode)this.children[1];
+    int left_val;
+    int right_val;
+
+    if(this.children[0].getClass().equals(ASTIdentifier.class)) {
+      ASTIdentifier left = (ASTIdentifier)this.children[0];
+      if (SemanticProcessor.symbols_table.get(left.ast_value) == null)
+        throw new RuntimeException("Variable in ASTIdentifier not previous declared");
+      
+      Symbol left_sym = SemanticProcessor.symbols_table.get(left.ast_value);
+
+      if (left_sym.type == "int") {
+        if (left_sym.init) {
+          left_val = left_sym.value;
+        } else throw new RuntimeException("Variable " + left.ast_value + " was not initialized");
+      } else throw new RuntimeException("Left child (" + left.ast_value + ") in ASTLess is not an Integer");
+    } //else if(arrayacess){}
+    else {
+      SimpleNode left = (SimpleNode)this.children[0];
+      left_val = left.process();
     }
-    return 1;
-    */
+
+    if(this.children[1].getClass().equals(ASTIdentifier.class)) {
+      ASTIdentifier right = (ASTIdentifier)this.children[1];
+      if (SemanticProcessor.symbols_table.get(right.ast_value) == null)
+        throw new RuntimeException("Variable in ASTIdentifier not previous declared");
+
+      Symbol right_sym = SemanticProcessor.symbols_table.get(right.ast_value);
+
+      if (right_sym.type == "int") {
+        if (right_sym.init) {
+          right_val = right_sym.value;
+        }else throw new RuntimeException("Variable " + right.ast_value + " was not initialized");
+      }
+      else
+        throw new RuntimeException("Right child (" + right.ast_value + ") in ASTLess is not an Integer");
+    } //else if(arrayacess){}
+    else{
+      SimpleNode right = (SimpleNode)this.children[1];
+      right_val = right.process();
+    } 
+    
+
+    return left_val + right_val;
   }
 
 
